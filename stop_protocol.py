3782713da9a0a8066782fbe11310c3c244afb7a2
Joaquin A. Zapata <147211045+Jazapata7705@users.noreply.github.com>
2024-03-23 14:26:57 -0500

    Add files via upload

---
+++ stop_protocol.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set GPIO numbering mode
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO pins for motor control
+IN1 = 17
+IN2 = 18
+
+# Set up GPIO pins
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+
+# Set up PWM for controlling motor speed
+pwm_frequency = 1000  # Frequency in Hz (1 kHz)
+motor_pwm = GPIO.PWM(IN1, pwm_frequency)
+
+# Function to drive motor forward at a given speed (0-100)
+def backward(speed):
+    motor_pwm.start(speed)
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+
+# Function to stop motor
+def stop():
+    motor_pwm.stop()
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.LOW)
+
+# Perform actions
+try:
+    stop()# Stop motor
+
+finally:
+    GPIO.cleanup()  # Clean up GPIO pins
