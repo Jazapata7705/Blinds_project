2807e1859203dd62413f1c2c1ce6f051d9d60078
Joaquin A. Zapata <147211045+Jazapata7705@users.noreply.github.com>
2024-03-23 15:48:25 -0500

    Update forward_protocol.py

--- forward_protocol.py
+++ forward_protocol.py
@@ -1,22 +1,23 @@
 import RPi.GPIO as GPIO
 import time
+import atexit
 
-# Set GPIO numbering mode
-GPIO.setmode(GPIO.BCM)
+
 
 # Define GPIO pins for motor control
 IN1 = 17
 IN2 = 18
 
-# Set up GPIO pins
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
+
 
 # Set up PWM for controlling motor speed
 pwm_frequency = 1000  # Frequency in Hz (1 kHz)
 motor_pwm = GPIO.PWM(IN1, pwm_frequency)
 
 # Function to drive motor forward at a given speed (0-100)
+def cleanup_gpio():
+    GPIO.cleanup()
+
 def forward(speed):
     motor_pwm.start(speed)
     GPIO.output(IN1, GPIO.HIGH)
@@ -28,5 +29,19 @@ def stop():
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.LOW)
 
-# Perform actions
-forward(10)     # Move motor forward at 50% speed
+
+
+def main():
+    # Set GPIO numbering mode
+    GPIO.setmode(GPIO.BCM)  
+
+    # Set up GPIO pins
+    GPIO.setup(IN1, GPIO.OUT)
+    GPIO.setup(IN2, GPIO.OUT)
+
+    atexit.register(cleanup_gpio())
+
+    forward(10)
+
+if __name__ == "__main__":
+    main()
